286aa07431333847cf7050b22398dc53619785d5
AbdulHameed Mohamed <59577985+AbdulHameedMohamed@users.noreply.github.com>
2023-12-17 11:53:04 +0200

    This is Our Sign Up & Login Feature Completed.

---
+++ src/util/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author w
+ */
+public class Constants {
+    public static final int PORT_NUMBER= 5005;
+    public static final int REGISTER = 1;
+    public static final int LOGIN = 2;
+    
+}
